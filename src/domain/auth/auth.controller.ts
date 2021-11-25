09db8fb18961bca28b084517e52d91e17fc54fc1
Tsimafei Kinevich <tsimafei.kinevich@itrexgroup.lan>
2021-11-25 16:29:26 +0300

    fix cognito

--- src/domain/auth/auth.controller.ts
+++ src/domain/auth/auth.controller.ts
@@ -66,10 +66,12 @@ export class AuthController implements OnModuleInit {
   ): Promise<IPatient> {
     try {
       await this.registerClass.register(createUserDto);
+      console.log('1');
       const user: CognitoUserSession = await this.loginClass.login({
         login: createUserDto.login,
         password: createUserDto.password,
       });
+      console.log('2');
       const userID: string = user.getAccessToken().payload.username;
       const createPatientMessage: ICreatePatientMessage = {
         name: createUserDto.name,
